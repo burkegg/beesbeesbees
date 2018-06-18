8b598208dd8bafd7541e5e38d8302a9ed31aa3e0
burkegg <burkegreen@gmail.com>
2018-06-18 11:58:04 -0700

    Grub, Bee, and HoneyMaker are finished.

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,12 @@
-var Bee = function() {
-};
 
+var Bee = function() {
+  Grub.call(this); // ask about this?
+  this.age = 5;
+  this.color = 'yellow';
+  this.job = 'keep on growing';
+  console.log(Grub.food);
+  
+};
+Bee.prototype = Object.create(Grub.prototype);
+
+Bee.prototype.constructor = Bee;
--- src/Grub.js
+++ src/Grub.js
@@ -1,3 +1,9 @@
+// Grub class.  
 var Grub = function() {
+  this.age = 0;
+  this.color = 'pink';
+  this.food = 'jelly';
 };
 
+// Add an eat method to grub prototype.
+Grub.prototype.eat = function(){};
